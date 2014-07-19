fa72215f8288f06c980981cac7b4ed5e7c17573d
John <johnhopkins@gmx.co.uk>
2014-07-19 23:00:30 +0100

    created table page and updated template.php

--- tables.php
+++ tables.php
@@ -7,7 +7,7 @@
 ?>
 <html>
 <head>
-<title>TITLE</title>
+<title>Tables</title>
 <meta charset="utf-8">
 <link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
 <link href="login.css" rel="stylesheet">
