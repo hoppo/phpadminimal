6fe514fd5620fb881bf5ad96f42382ca1c620bb2
John <johnhopkins@gmx.co.uk>
2014-07-19 21:19:05 +0100

    added navbar code and also created an empty template file

---
+++ template.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>TITLE</title>
+<meta charset="utf-8">
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
+<link href="login.css" rel="stylesheet">
+<script src="js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+
+
+</tbody>
+</table>
+</body>
+</html>
+
