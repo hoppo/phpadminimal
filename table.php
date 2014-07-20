00fe3d80e2f156ef6cc71447fc7206826f72cb2d
John <johnhopkins@gmx.co.uk>
2014-07-20 17:34:43 +0100

    added table.php eith basic functionality

---
+++ table.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<?php
+    //bring in the functions file to utilise all the useful stuff
+    include 'functions.php';
+    session_start();
+    $_SESSION['table']=$_GET['table'];
+    if (isset($_GET['log_out'])) logOut();
+?>
+<html>
+<head>
+<title>Table</title>
+<meta charset="utf-8">
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css" rel="stylesheet">
+<script src="js/bootstrap.min.js"></script>
+</head>
+
+<body>
+<?php
+    //include navbar code
+    require($DOCUMENT_ROOT . "navbar.html");
+?>
+
+Display table
+<?php
+$table = $_GET['table'];
+echo $table;
+?>
+</tbody>
+</table>
+</body>
+</html>
+
